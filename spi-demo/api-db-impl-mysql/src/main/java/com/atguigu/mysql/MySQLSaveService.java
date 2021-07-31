9c936d44f765bcaaaaa259aea8b47b05017fea76
koala <1129269131@qq.com>
2021-07-31 12:14:10 +0800

    /修改注释

--- spi-demo/api-db-impl-mysql/src/main/java/com/atguigu/mysql/MySQLSaveService.java
+++ spi-demo/api-db-impl-mysql/src/main/java/com/atguigu/mysql/MySQLSaveService.java
@@ -2,10 +2,11 @@ package com.atguigu.mysql;
 
 import com.atguigu.data.DataSaveService;
 
-public class MySQLSaveService implements DataSaveService
-{
+public class MySQLSaveService implements DataSaveService {
+
     @Override
     public void saveData(String data) {
-        System.out.println("MySQL保存了数据......."+data);
+        System.out.println("MySQL保存了数据......." + data);
     }
+
 }
--- spi-demo/api-db-impl-redis/src/main/java/com/atguigu/redis/RedisSaveService.java
+++ spi-demo/api-db-impl-redis/src/main/java/com/atguigu/redis/RedisSaveService.java
@@ -3,8 +3,10 @@ package com.atguigu.redis;
 import com.atguigu.data.DataSaveService;
 
 public class RedisSaveService implements DataSaveService {
+
     @Override
     public void saveData(String data) {
-        System.out.println("Redis保存了数据......."+data);
+        System.out.println("Redis保存了数据......." + data);
     }
+
 }
